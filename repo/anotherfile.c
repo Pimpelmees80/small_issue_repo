7d077e93bee523dec7e761a520a14d29871f0485
Pimpelmees80 <167314317+Pimpelmees80@users.noreply.github.com>
2026-07-04 14:49:31 +0200

    changin two files

---
+++ repo/anotherfile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int classify(int n)
+{
+    if (n > 0)                    // Decision 1
+    {
+			return 2
+    }
+    else if (n < 0)               // Decision 3
+    {
+        return 3;                 // Negative
+    }
+    else
+    {
+        return 4;                 // Zero
+    }
+}
